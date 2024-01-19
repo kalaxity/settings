7fe19cfea75710fa9e7533bcee46f20181196b38
kalaxity <61872026+kalaxity@users.noreply.github.com>
2024-01-19 22:52:00 +0900

    Create init_wsl.sh

---
+++ init_wsl.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# WSL2の初期設定スクリプト
+
+# windowsのDownloadsフォルダにシンボリックリンクを張る
+ln -s /mnt/c/Users/$(powershell.exe "Write-Host \$env:username")/Downloads ./win_downloads
+
+# apt関係
+sudo apt update
+sudo apt install build-essential gdb git netdiscover nmap python3 tree unar zsh curl vim wget -y
+sudo apt upgrade -y
+
+# zsh関係: vimrcどうするかも検討したい
+wget https://raw.githubusercontent.com/kalaxity/vimrc/main/.zshrc
+sudo chsh -s $(which zsh) $(whoami)
